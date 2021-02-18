7e5dadf09ecd0095a4146ddeeb0003f23f0bf187
Snowapril <sinjihng@naver.com>
2021-02-18 11:29:05 +0900

    feat(add-dep) : add cxxopts dependency for simple-weight cmd line parse

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <cxxopts/cxxopts.hpp>
+
 
 int main(int argc, char* argv[])
 {
-	(void)argc;(void)argv;
+	cxxopts::Options options("modern-opengl-template", "simple description");
+
+	options.add_options()
+		("d,debug", "Enable debugging") // a bool parameter
+		("i,integer", "Int param", cxxopts::value<int>())
+		("f,file", "File name", cxxopts::value<std::string>())
+		("v,verbose", "Verbose output", cxxopts::value<bool>()->default_value("false"));
+
+	auto result = options.parse(argc, argv);
+
+	if (result.count("help"))
+	{
+		std::cout << options.help() << std::endl;
+		exit(0);
+	}
+
 	return 0;
 }
