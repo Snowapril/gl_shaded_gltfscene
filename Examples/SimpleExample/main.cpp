e469262d32262e84c48473427436dd6cfe880f3c
Snowapril <sinjihng@naver.com>
2021-02-18 11:27:29 +0900

    feat(project-setup) : remove examples and make main as executable build

--- Examples/SimpleExample/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(int argc, char* argv[])
-{
-	(void)argc; (void)argv;
-	std::cout << "Simple example" << std::endl;
-}
--- Tests/UnitTests/SimpleTest.cpp
+++
@@ -1,7 +0,0 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include <doctest/doctest.h>
-
-TEST_CASE("Simple test")
-{
-    CHECK(5 == 5);
-}
