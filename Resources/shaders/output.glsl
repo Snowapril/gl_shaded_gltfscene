b800e228b5833ac2741868abdd24de3aa054cf49
snowapril <sinjihng@naver.com>
2021-05-08 22:12:56 +0900

    feat : fix GL_ARB_shading_language_include error

--- Resources/shaders/output.glsl
+++ Resources/shaders/output.glsl
@@ -9,9 +9,10 @@ layout(location = 1) in VSOUT
 } fs_in;
 layout(location = 0) out vec4 fragColor;
 
-#include "gltf.glsl"
+#include gltf.glsl
+uniform GltfShadeMaterial material;
 
 void main()
 {
-	fragColor = vec4(vec3(0.0f, 0.0f, 0.9f), 1.0f);
+	fragColor = vec4(vec3(0.0f, 0.0f, 0.0f), 1.0f);
 }
