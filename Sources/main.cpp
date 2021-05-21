0e7305fce76988230174757594b9c546774e0ace
snowapril <sinjihng@naver.com>
2021-05-21 22:47:54 +0900

    feat : rename cxxopts options title & name

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include <cxxopts/cxxopts.hpp>
 
-#include <SampleRenderer.hpp>
+#include <GLTFSceneRenderer.hpp>
 #include <GL3/Window.hpp>
 #include <GLFW/glfw3.h>
 #include <chrono>
 
 int main(int argc, char* argv[])
 {
-	cxxopts::Options options("modern-opengl-template", "simple description");
+	cxxopts::Options options("gl_shaded_gltfscene", "simple description");
 
 	options.add_options()
-		("t,title", "Window Title(default is 'modern-opengl-template')", cxxopts::value<std::string>()->default_value("modern-opengl-template"))
+		("t,title", "Window Title(default is 'gl_shaded_gltfscene')", cxxopts::value<std::string>()->default_value("gl_shaded_gltfscene"))
 		("w,width", "Window width(default is 1200)", cxxopts::value<int>()->default_value("1200"))
 		("h,height", "Window height(default is 900)", cxxopts::value<int>()->default_value("900"))
 		("s,scene", "GLTF Scene filepath(default is '" RESOURCES_DIR "scenes/FlightHelmet/FlightHelmet.gltf')", 
 			cxxopts::value<std::string>()->default_value(RESOURCES_DIR "scenes/FlightHelmet/FlightHelmet.gltf"))
 		("e,envmap", "HDR SkyDome image filepath(default is '" RESOURCES_DIR  "scenes/environment.hdr')",
-			cxxopts::value<std::string>()->default_value(RESOURCES_DIR "scenes/environment.hdr"));
-
+			cxxopts::value<std::string>()->default_value(RESOURCES_DIR "scenes/environment.hdr"))
+		("h,help", "Program usage");
+		
 	auto result = options.parse(argc, argv);
 
 	if (result.count("help"))
@@ -27,7 +28,7 @@ int main(int argc, char* argv[])
 		exit(0);
 	}
 
-	auto renderer = std::make_unique<SampleRenderer>();
+	auto renderer = std::make_unique<GLTFSceneRenderer>();
 	if (!renderer->Initialize(result))
 	{
 		std::cerr << "Failed to initialize the Renderer" << std::endl;
