a339f5bd7ba41b7d36a862ddfe7d83a337d3a4a1
snowapril <sinjihng@naver.com>
2021-05-21 23:13:51 +0900

    feat : modify duplicated CLI argument options

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,25 +1,25 @@
-#include <iostream>
 #include <cxxopts/cxxopts.hpp>
 
 #include <GLTFSceneRenderer.hpp>
 #include <GL3/Window.hpp>
 #include <GLFW/glfw3.h>
 #include <chrono>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
-	cxxopts::Options options("gl_shaded_gltfscene", "simple description");
+	cxxopts::Options options(argv[0], "simple description");
 
 	options.add_options()
 		("t,title", "Window Title(default is 'gl_shaded_gltfscene')", cxxopts::value<std::string>()->default_value("gl_shaded_gltfscene"))
-		("w,width", "Window width(default is 1200)", cxxopts::value<int>()->default_value("1200"))
-		("h,height", "Window height(default is 900)", cxxopts::value<int>()->default_value("900"))
-		("s,scene", "GLTF Scene filepath(default is '" RESOURCES_DIR "scenes/FlightHelmet/FlightHelmet.gltf')", 
+		("x,width", "Window width (default is 1200)", cxxopts::value<int>()->default_value("1200"))
+		("y,height", "Window height (default is 900)", cxxopts::value<int>()->default_value("900"))
+		("s,scene", "GLTF Scene filepath(default is '" RESOURCES_DIR "scenes/FlightHelmet/FlightHelmet.gltf')",
 			cxxopts::value<std::string>()->default_value(RESOURCES_DIR "scenes/FlightHelmet/FlightHelmet.gltf"))
 		("e,envmap", "HDR SkyDome image filepath(default is '" RESOURCES_DIR  "scenes/environment.hdr')",
 			cxxopts::value<std::string>()->default_value(RESOURCES_DIR "scenes/environment.hdr"))
-		("h,help", "Program usage");
-		
+		("h,help", "Print usage");
+
 	auto result = options.parse(argc, argv);
 
 	if (result.count("help"))
