3bdc56908677ea72eae3d850f5a89e8360128252
snowapril <sinjihng@naver.com>
2021-05-26 11:55:16 +0900

    feat : align GltfShadeMaterial struct to std430 layout

--- Resources/shaders/gltf.glsl
+++ Resources/shaders/gltf.glsl
@@ -1,29 +1,31 @@
 struct GltfShadeMaterial
 {
-	int shadingModel;  // 0: metallic-roughness, 1: specular-glossiness
-
 	// PbrMetallicRoughness
-	vec4  pbrBaseColorFactor;
-	int   pbrBaseColorTexture;
-	float pbrMetallicFactor;
-	float pbrRoughnessFactor;
-	int   pbrMetallicRoughnessTexture;
+	vec4  pbrBaseColorFactor; //16
+
+	int   pbrBaseColorTexture; //20
+	float pbrMetallicFactor; //24
+	float pbrRoughnessFactor; //28 
+	int   pbrMetallicRoughnessTexture; //32
 
 	// KHR_materials_pbrSpecularGlossiness
-	vec4  khrDiffuseFactor;
-	int   khrDiffuseTexture;
-	vec3  khrSpecularFactor;
-	float khrGlossinessFactor;
-	int   khrSpecularGlossinessTexture;
+	vec4  khrDiffuseFactor; //48
 
-	int   emissiveTexture;
-	vec3  emissiveFactor;
-	int   alphaMode;
-	float alphaCutoff;
-	int   doubleSided;
+	vec3  khrSpecularFactor; //60
+	int   khrDiffuseTexture; //64
+	float khrGlossinessFactor; //68
+	int   khrSpecularGlossinessTexture; //72
 
-	int   normalTexture;
-	float normalTextureScale;
-	int   occlusionTexture;
-	float occlusionTextureStrength;
+	int   emissiveTexture; //76
+	int   alphaMode; //80
+	vec3  emissiveFactor; //92
+	float alphaCutoff; // 96
+	int   doubleSided; //100
+
+	int   normalTexture; //104
+	float normalTextureScale; //108
+	int   occlusionTexture; //112
+	float occlusionTextureStrength; //116
+	int shadingModel;  // 120, 0: metallic-roughness, 1: specular-glossiness 
+	int padding[2];
 };
