e35ad35e8a0177dfae2f44dcfd2cafb66e35bca1
snowapril <sinjihng@gmail.com>
2021-07-05 19:03:13 +0900

    feat : modify default camera position & direction

--- Sources/GLTFSceneApp.cpp
+++ Sources/GLTFSceneApp.cpp
@@ -27,7 +27,7 @@ bool GLTFSceneApp::OnInitialize(std::shared_ptr<GL3::Window> window, const cxxop
 	if (!defaultCam->SetupUniformBuffer())
 		return false;
 
-	defaultCam->SetupCamera(glm::vec3(0.0f, 0.0f, -5.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	defaultCam->SetupCamera(glm::vec3(-3.0f, 0.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	defaultCam->SetProperties(window->GetAspectRatio(), 60.0f, 0.001f, 100.0f);
 	defaultCam->UpdateMatrix();
 
